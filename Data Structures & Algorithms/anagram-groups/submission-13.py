b2512b03db743261617e06a1cda03812732d3ea1
ay-2814 <mailtoayushman@gmail.com>
2026-07-15 00:17:57 -0700

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        le_group = defaultdict(list)
+
+        for s in strs:
+            char_map = [0]*26
+            for c in s:
+                char_map[ord(c) - ord('a')] += 1
+            
+            le_group[tuple(char_map)].append(s)
+
+        return list(le_group.values())
+
+        
+
+
+        
